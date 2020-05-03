b3b1228c7771f897f8de77bc57eebaa6e9cd416a
Brad Hawkins <github@monkeylaw.org>
2020-05-03 10:41:52 -0500

    Additional editing on Help

--- src/Help.js
+++ src/Help.js
@@ -8,28 +8,13 @@ function Help(props) {
   return(
     <div className="help-panel">
       <h3>Help</h3>
-      <p>Select players by clicking their names. Assign points using key commands below. Accidentally eliminated players can be reinstated by selecting their name again.</p>
+      <p>Select players by clicking their names. Assign points using key commands below.</p>
+      <p>Accidentally eliminated players can be reinstated by reselecting their name.</p>
       <h4>Key Commands</h4>
       <table className="key-commands">
         <tr>
-					<td><span className="help-key">1</span></td>
-					<td>Assign selected players a 1</td>
-				</tr>
-        <tr>
-					<td><span className="help-key">2</span></td>
-					<td>Assign selected players a 2</td>
-				</tr>
-        <tr>
-					<td><span className="help-key">3</span></td>
-					<td>Assign selected players a 3</td>
-				</tr>
-        <tr>
-					<td><span className="help-key">4</span></td>
-					<td>Assign selected players a 4</td>
-				</tr>
-        <tr>
-					<td><span className="help-key">5</span></td>
-					<td>Assign selected players a 5</td>
+					<td><span className="help-key">1</span>&nbsp;...&nbsp;<span className="help-key">5</span></td>
+					<td>Assign score to selected players</td>
 				</tr>
         <tr>
 					<td><span className="help-key"><FontAwesomeIcon icon={faArrowRight}/></span> or <span className="help-key">+</span></td>
@@ -45,7 +30,7 @@ function Help(props) {
 				</tr>
         <tr>
 					<td><span className="help-key">E</span></td>
-					<td>Eliminate selected players (tap player card again to undo)</td>
+					<td>Eliminate selected players</td>
 				</tr>
         <tr>
 					<td><span className="help-key">S</span></td>
